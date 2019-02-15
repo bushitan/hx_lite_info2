c6ae6c02f045477c502946e053d7df9bc9a9e382
bushitan <373514952@qq.com>
2019-02-15 20:37:58 +0800

    1.3.3

--- pages/roster/roster.js
+++ pages/roster/roster.js
@@ -24,6 +24,7 @@ Page({
             GP.onInit()
     },
     onInit(){
+        pageNO = 1
         GP.setData({
             rosterList: [],
             industryID: wx.getStorageSync(API.KEY_INDUSTRYID),
